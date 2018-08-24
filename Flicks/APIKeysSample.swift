57043852d7ec582b12ffa9e91aa0cbe2d61d67a5
Victor Li <victorli5674@yahoo.com>
2018-08-23 23:02:55 -0700

    Able to request to Movie Database API and created a API keys sample file

---
+++ Flicks/APIKeysSample.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeysSample.swift
+//  Flicks
+//
+//  Created by Victor Li on 8/23/18.
+//  Copyright © 2018 Victor Li. All rights reserved.
+//
+
+import Foundation
+
+// make sure to change enum name to APIKeys
+enum APIKeysSample: String {
+    case MOVIE_DATABASE = ""
+}
