cc28933dd94db1e8e69e451cabbeef5556dd3a4a
Victor Li <victorli5674@yahoo.com>
2018-09-17 08:26:04 -0700

    fixed bug where it was not showing the right movie when filtering through movies

--- Flix/ViewControllers/NowPlayingViewController.swift
+++ Flix/ViewControllers/NowPlayingViewController.swift
@@ -166,7 +166,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UISearc
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UITableViewCell
         if let indexPath = tableView.indexPath(for: cell) {
-            let movie = movies[indexPath.row]
+            let movie = filteredMovies[indexPath.row]
             let detailViewController = segue.destination as! DetailViewController
             detailViewController.movie = movie
         }
